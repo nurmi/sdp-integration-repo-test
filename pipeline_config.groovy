17f3f4eb7c440f75f708d937577c8363c05ecc57
Daniel Nurmi <nurmi@anchore.com>
2020-05-12 13:29:18 -0700

    init
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

---
+++ pipeline_config.groovy
@@ -0,0 +1,30 @@
+// restrict individual repository Jenkinsfiles
+allow_scm_jenkinsfile = true
+// skip the default JTE checkout and do it explicitly
+skip_default_checkout = true
+// define application environment objects
+application_environments{
+    dev{
+        long_name = "Development"
+    }
+    prod{
+        long_name = "Production"
+    }
+}
+
+/*
+  define libraries to load.
+  available libraries are based upon the
+  library sources configured.
+*/
+libraries{
+    github
+    sonarqube
+    docker
+    // library configurations are determined by
+    // the specific library implementation
+    openshift{
+        url = "https://example.openshift.com"
+        cred_id = "openshift"
+    }
+}
