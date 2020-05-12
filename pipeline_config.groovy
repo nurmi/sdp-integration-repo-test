653d49703aa1fb25e47ea56f65cb0d95a69f5ef0
Daniel Nurmi <nurmi@anchore.com>
2020-05-12 13:59:41 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,8 +1,9 @@
 // restrict individual repository Jenkinsfiles
-allow_scm_jenkinsfile = true
+//allow_scm_jenkinsfile = true
 // skip the default JTE checkout and do it explicitly
-skip_default_checkout = true
+//skip_default_checkout = true
 // define application environment objects
+
 application_environments{
     dev{
         long_name = "Development"
@@ -21,7 +22,9 @@ libraries{
     //github
     //sonarqube
     //maven
-    docker
+    docker {
+      repo_path_prefix = "my-app-images"
+    }
     // library configurations are determined by
     // the specific library implementation
     //openshift{
