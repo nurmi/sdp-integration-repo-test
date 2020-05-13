caeb0c29acf0478af5cedb5edcc6ead346c0ee44
Daniel Nurmi <nurmi@anchore.com>
2020-05-13 16:24:55 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- libraries/anchore_scan/scan_container_image.groovy
+++ libraries/anchore_scan/scan_container_image.groovy
@@ -11,7 +11,17 @@ void call(){
         withCredentials([usernamePassword(credentialsId: config.cred, passwordVariable: 'pass', usernameVariable: 'user')]) {
                 String url = "${anchore_engine_base_url}/system/"
 		sh "echo curl -u '${user}:${pass}' ${url}"
-		sh "curl -u '${user}:${pass}' ${url}"		
+		sh "curl -u '${user}:${pass}' ${url}"
+
+                def images = get_images_to_build()
+                images.each{ img ->
+		  url = "${anchore_engine_base_url}/images"
+		  def new_image = [tag: "${img.registry}/${img.repo}:${img.tag}"]
+		  def new_image_json = JsonOutput.toJson(new_image)
+		  sh "echo curl -u -H 'content-type: application/json' -X POST '${user}:${pass}' ${url} -d '${new_image_json}'"
+                  //sh "docker build ${img.context} -t ${img.registry}/${img.repo}:${img.tag}"
+                  //sh "docker push ${img.registry}/${img.repo}:${img.tag}"
+                }
 	}  	 
       }
     }
