08463de266dc9c8a66fd0dc0d806dcea60e77ab3
Daniel Nurmi <nurmi@anchore.com>
2020-05-13 17:07:04 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- libraries/anchore_scan/scan_container_image.groovy
+++ libraries/anchore_scan/scan_container_image.groovy
@@ -26,13 +26,13 @@ void call(){
 		  def input_image_json = JsonOutput.toJson(input_image)
 		  sh "echo curl -u '${user}':'${pass}' -H 'content-type: application/json' -X POST ${url} -d '${input_image_json}'"		  
 		  sh "curl -u '${user}':'${pass}' -H 'content-type: application/json' -X POST ${url} -d '${input_image_json}' > new_image.json"
-          	  archiveArtifacts 'new_image.json'
-		  def new_image = this.parse_json("new_image.json")
+
+		  def new_image = this.parse_json("new_image.json")[0]
 		  Boolean done = false
  		  url = "${anchore_engine_base_url}/images/${new_image.imageDigest}"		  
 		  while(!done) {
 		    sh "curl -u '${user}':'${pass}' -H 'content-type: application/json' -X GET ${url} > new_image_check.json"
-		    def new_image_check = parse_json("new_image_check.json")
+		    def new_image_check = this.parse_json("new_image_check.json")[0]
 		    sh "echo ${new_image_check.analysis_status}"
 		    if (new_image_check.analysis_status == "analyzed") {
 		      done = true
