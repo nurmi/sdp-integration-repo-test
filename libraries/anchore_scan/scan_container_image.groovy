07f6201a193271b26cc2a168baac6be68f2d8de1
Daniel Nurmi <nurmi@anchore.com>
2020-05-14 12:22:37 -0700

    update
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- libraries/anchore_scan/scan_container_image.groovy
+++ libraries/anchore_scan/scan_container_image.groovy
@@ -107,7 +107,7 @@ String anchore_engine_base_url = config.anchore_engine_url
   
   try {
     http_result = "anchore_results/anchore_policy_evaluations.json"
-    url = "${anchore_engine_base_url}/images/${image_digest}/check?history=false&detail=false&tag=${input_image_fulltag}"
+    url = "${anchore_engine_base_url}/images/${image_digest}/check?history=false&detail=true&tag=${input_image_fulltag}"
     sh "curl -u '${user}':'${pass}' -H 'content-type: application/json' -o ${http_result} '${url}'"
     /*
     String cmd = "curl -u '${user}':'${pass}' -H 'content-type: application/json' -o ${http_result} '${url}'"
